300d2047b4a947d226baabecd3348a3597411bcc
Boy Kai <JToolesiii@gmail.com>
2017-01-24 22:32:06 -0500

    Feature: Add linear SVM classifier in svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,19 +12,39 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+### create linear classifier
+clf = SVC(kernel = 'linear')
+
+### start timer
+t0 = time()
+
+### fit the classifier on the training features and labels
+clf.fit(features_train, labels_train)
+
+### stop timer
+print('Training Time: ', round(time() - t0, 3), "s")
+
+### start timer
+t1 = time()
+
+### use the trained classifier to predict labels for the test features
+pred = clf.predict(features_test)
+
+### stop timer
+print('Prediction Time: ', round(time() - t1, 3), "s")
+
+### calculate and return the accuracy on the test data
+accuracy = clf.score(features_test, labels_test)
 
 
+print(accuracy)
 
-#########################################################
-### your code goes here ###
-
-#########################################################
 
 
