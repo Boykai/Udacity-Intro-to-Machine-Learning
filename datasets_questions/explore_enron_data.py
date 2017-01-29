6f6cb43ef1a0b00b17c78bc37aa153239a0a862e
Boy Kai <JToolesiii@gmail.com>
2017-01-29 12:00:47 -0500

    Feature: Add percentage of NaN for total_payments print statement

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -23,9 +23,11 @@ enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r")
 # Print the number of rows/people in the Enron dataset
 print('Number of data points = ' + str(len(enron_data)))
 
+
 # Print the number of unique features in the Enron dataset for each person
 print('Number of features for each data point = ' 
       + str(len(enron_data.values()[0])))
+
 
 # Print the person of interest count for the data set
 poi_count = 0
@@ -36,6 +38,7 @@ for key in enron_data.keys():
         
 print('Total number of people of interest in data set = ' + str(poi_count))
 
+
 # Print the total value of the stock belonging to James Prentice
 print('Total number value of the stock belonging to James Prentice = '
       + str(enron_data['PRENTICE JAMES']['total_stock_value']))
@@ -43,6 +46,7 @@ print('Total number value of the stock belonging to James Prentice = '
 # Print the value of stock options exercised by Jeff Skilling
 print('Value of stock options exercised by Jeffrey Skilling = '
       + str(enron_data['SKILLING JEFFREY K']['exercised_stock_options']))
+
 
 # Print "Of these three individuals (Lay, Skilling, and Fastow), who took home
 # the most money (largest value of "total_payments" feature)? How much money
@@ -63,6 +67,7 @@ print('Fastow took home $'
 print('Lay received the most money at $'
       + str(lay_take_home_pay))
 
+
 # Print how many folks in the dataset have a quantified salary
 enron_df = pd.DataFrame(enron_data).T
 
@@ -73,3 +78,10 @@ print('Number of non-NaN salaries = '
 # Print how many folk in the dataset have email addresses
 print('Number of non-NaN email addresses = '
       + str(enron_rows - enron_df['email_address'].isin(['NaN']).sum()))
+
+
+# Print the percentage of people in the dataset that have "NaN" for their
+# total payments
+print('Percentage of people that have NaN for their total payments = '
+      + str(float(enron_df['total_payments'].isin(['NaN']).sum())/float(enron_rows) * 100.0)
+      + '%')
