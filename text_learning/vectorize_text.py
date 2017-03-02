c898e09ec2a7c6ca903fa22a886e9bfe023bf309
Boy Kai <JToolesiii@gmail.com>
2017-03-01 20:39:36 -0500

    Feature: Remove 'sshacklensf' string from emails

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -53,7 +53,8 @@ for name, from_person in [('sara', from_sara), ('chris', from_chris)]:
             
             ### use str.replace() to remove any instances of the words
             ### ['sara', 'shackleton', 'chris', 'germani']
-            words_to_delete = ['sara', 'shackleton', 'chris', 'germani']
+            words_to_delete = ['sara', 'shackleton', 'chris', 'germani', 
+                               'sshacklensf']
             for word in words_to_delete:
                 current_email = current_email.replace(word, '')
             
