1ce97dfd0c6c267eac68607e0452289876ef2fae
Boy Kai <JToolesiii@gmail.com>
2017-03-08 13:58:59 -0500

    Refactor: Add getFeature method to return list of features

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -23,17 +23,22 @@ from sklearn.cross_validation import StratifiedShuffleSplit
 2. After attempting to run feature_format, removed 'email_address' feature
    due to this feature throwing an error
 '''
-
-# Create feature list to include needed features for classifer
-# 'poi' must be first feature within the list
-# Features removed later in KBest and PCA pipeline
-features_list = ['poi', 'salary', 'to_messages', 'deferral_payments',
-                 'total_payments', 'exercised_stock_options', 'bonus',
-                 'restricted_stock', 'shared_receipt_with_poi',
-                 'restricted_stock_deferred', 'total_stock_value',
-                 'expenses', 'loan_advances', 'from_messages', 'other',
-                 'from_this_person_to_poi', 'director_fees', 'deferred_income',
-                 'long_term_incentive', 'from_poi_to_this_person']
+def getFeatureList():
+    '''
+    Creates list of labels for features of Enron dataset
+    @return: features_list (a list of strings)
+    '''
+    # Create feature list to include needed features for classifer
+    # 'poi' must be first feature within the list
+    # Features removed later in KBest and PCA pipeline
+    features_list = ['poi', 'salary', 'to_messages', 'deferral_payments',
+                     'total_payments', 'exercised_stock_options', 'bonus',
+                     'restricted_stock', 'shared_receipt_with_poi',
+                     'restricted_stock_deferred', 'total_stock_value',
+                     'expenses', 'loan_advances', 'from_messages', 'other',
+                     'from_this_person_to_poi', 'director_fees', 'deferred_income',
+                     'long_term_incentive', 'from_poi_to_this_person']
+    return features_list
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -106,6 +111,7 @@ def evaluateClf(classifer, feats_test, labs_test, predictions):
         Precision
         Recall
         F1 Score
+        ROC Curve AUC
         
         classifer: ML classifer model object (an object)
         
